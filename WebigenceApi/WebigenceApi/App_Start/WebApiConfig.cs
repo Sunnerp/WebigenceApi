afa30ec87e31c487bc10fc040a182864b23d5300
Pav Sunner <sunnerpav@gmail.com>
2021-05-02 13:59:03 +0100

    changed route

--- WebigenceApi/WebigenceApi/App_Start/WebApiConfig.cs
+++ WebigenceApi/WebigenceApi/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace WebigenceApi
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
