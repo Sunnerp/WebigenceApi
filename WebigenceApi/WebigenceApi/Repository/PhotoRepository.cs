34d17e1f301c461f0c4f157c2104bacaf438988e
Pav Sunner <sunnerpav@gmail.com>
2021-05-02 14:20:30 +0100

    added http call to get json from the photos api

--- WebigenceApi/WebigenceApi/Repository/PhotoRepository.cs
+++ WebigenceApi/WebigenceApi/Repository/PhotoRepository.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
+using WebigenceApi.Models;
 
 namespace WebigenceApi.Repository
 {
     public class PhotoRepository
     {
+        private List<Photo> photos;
+
+        public PhotoRepository()
+        {
+            string json = getJson();
+            photos = JsonConvert.DeserializeObject<List<Photo>>(json);
+        }
+        public List<Photo> getPhotos()
+        {
+            return photos;
+        }
+        private string getJson()
+        {
+            string html = string.Empty;
+            string url = @"http://jsonplaceholder.typicode.com/photos";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            return html;
+
+        }
+
     }
 }
--- WebigenceApi/WebigenceApi/Service/PhotoGet.cs
+++ WebigenceApi/WebigenceApi/Service/PhotoGet.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebigenceApi.Models;
+using WebigenceApi.Repository;
 
 namespace WebigenceApi.Service
 {
     public class PhotoGet
     {
+        private PhotoRepository photoRepository;
+
+        public PhotoGet()
+        {
+            photoRepository = new PhotoRepository();
+        }
+
+        public List<Photo> getModels()
+        {
+            return photoRepository.getPhotos();
+        }
     }
 }
