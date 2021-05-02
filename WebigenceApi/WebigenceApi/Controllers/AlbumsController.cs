e66b29dd3b957d4d56378f2f01b5314b61e598cb
Pav Sunner <sunnerpav@gmail.com>
2021-05-02 13:58:18 +0100

    added constructors and references to methods to GET json and return to user

--- WebigenceApi/WebigenceApi/Controllers/AlbumsController.cs
+++ WebigenceApi/WebigenceApi/Controllers/AlbumsController.cs
@@ -1,24 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using WebigenceApi.Models;
+using WebigenceApi.Service;
 
 namespace WebigenceApi.Controllers
 {
     public class AlbumsController : ApiController
     {
-        // GET: api/Albums ** Returns whole album responses
-        public IEnumerable<string> Get()
+        private AlbumGet albumService;
+
+        public AlbumsController()
         {
-            return new string[] { "value1", "value2" };
+            albumService = new AlbumGet();
+        }
+
+        // GET: api/Albums ** Returns whole album responses
+        public List<Album> Get()
+        {
+            return (albumService.getModels());
         }
 
         // GET: api/Albums/5 ** Returns specfic album based on ID given
-        public string Get(int id)
+        public List<Album> Get(int id)
         {
-            return "value";
+            return albumService.getModels().Where(obj => obj.userId == id).ToList();
         }
     }
 }
