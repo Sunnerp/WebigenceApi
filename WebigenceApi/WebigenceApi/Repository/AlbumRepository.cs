4defdba79b8cc35cd5fa3af6ff91ec7509d0c111
Pav Sunner <sunnerpav@gmail.com>
2021-05-02 13:43:19 +0100

    create repo and service to get and set the data for albums using a http call

---
+++ WebigenceApi/WebigenceApi/Repository/AlbumRepository.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using WebigenceApi.Models;
+
+namespace WebigenceApi.Repository
+{
+    public class AlbumRepository
+    {
+        private List<Album> albums;
+
+        public AlbumRepository()
+        {
+            string json = getJson();
+
+            albums = JsonConvert.DeserializeObject<List<Album>>(json);
+        }
+
+        public List<Album> getAlbums()
+        {
+            return albums;
+        }
+
+        private string getJson()
+        {
+            string html = string.Empty;
+            string url = @"http://jsonplaceholder.typicode.com/albums";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            return html;
+
+        }
+
+    }
+}
---
+++ WebigenceApi/WebigenceApi/Service/AlbumGet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebigenceApi.Models;
+using WebigenceApi.Repository;
+
+namespace WebigenceApi.Service
+{
+    public class AlbumGet
+    {
+        private AlbumRepository albumRepository;
+
+        public AlbumGet()
+        {
+            albumRepository = new AlbumRepository();
+        }
+
+        public List<Album> getModels()
+        {
+            return albumRepository.getAlbums();
+        }
+    }
+}
